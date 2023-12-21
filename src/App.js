a5f395703a4db1d78c5c52d4f10620e1f4848809
Vundela Vamsi <vr313009@gmail.com>
2023-12-21 18:41:21 +0000

    Create Authentication App using ReactJS

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Switch, Route} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <Route exact path="/" component={Home} />
+    <Route component={NotFound} />.
+  </Switch>
+)
+
+export default App
